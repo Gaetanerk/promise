259d57ac195aa39888bbb0114a7c6e61bf69eada
Gaetanerk <gaetan.turben@outlook.com>
2022-05-11 09:35:41 +0200

    tbug pays sans capitale

--- assets/script.js
+++ assets/script.js
@@ -1,54 +1,74 @@
-let result = document.querySelector("#result")
+let result = document.querySelector("#result");
 
-document.querySelector('#searchCountry').onsubmit = function (e) {
-    document.querySelector("#result").innerHTML = ""
-    e.preventDefault();
-    actionForm(document.querySelector('#country').value);
-}
+document.querySelector("#searchCountry").onsubmit = function (e) {
+  document.querySelector("#result").innerHTML = "";
+  e.preventDefault();
+  actionForm(document.querySelector("#country").value);
+};
 
 const actionForm = async (name) => {
-    let url = `https://restcountries.com/v3.1/name/${name}`
-    let response = await fetch(url);
-    let data = await response.json();
-    console.log(data);
+  let url = `https://restcountries.com/v3.1/name/${name}`;
+  let response = await fetch(url);
+  let data = await response.json();
+  console.log(data);
 
-    data.forEach((country) => {
-        const img = document.createElement('img');
-        img.src = `${country.flags.png}`;
-        img.classList.add("rounded");
-        img.style = "border: 1px solid black; width: 10vw";
-        const p = document.createElement('p');
-        p.classList.add("mb-2", "fs-2", "fw-bold");
-        const p2 = document.createElement('p');
-        p2.classList.add('mt-2')
-        const p3 = document.createElement('p');
-        p3.classList.add("mx-auto");
-        p3.style = "border: 1px solid black; width: 10vw"
-        let lstLng = [];
-        for (lang in country.languages) {
-            lstLng.push(country.languages[lang]);
-        }
-        let lstCur = [];
-        for (currencie in country.currencies) {
-            lstCur.push(country.currencies[currencie]);
-        }
-        const p4 = document.createElement('p');
-        lstCur.forEach((cur) => {
-            if (country.capital) {
-                p.innerHTML = `${country.name.common}`;
-                p2.innerHTML = `Capitale : ${country.capital[0]}  <br/>  Habitants : ${country.population} <br/> Nom en francais : ${country.translations.fra.official}
-                <br/> Superficie : ${country.area} km² <br/> Langues : ${lstLng.join(", ")} <br/> Monnaie : 
-                ${cur["name"]} <br/> Symbole : ${cur["symbol"]}`;
-            }
-            else {
-                p.textContent = `${country.name.common} - Pas de capitale`
-            }
-        });
-        result.appendChild(p);
-        result.appendChild(img);
-        result.appendChild(p2);
-        result.appendChild(p4);
-        result.appendChild(p3);
-        console.log(country);
+  data.forEach((country) => {
+    const img = document.createElement("img");
+    img.src = `${country.flags.png}`;
+    img.classList.add("rounded");
+    img.style = "border: 1px solid black; width: 10vw";
+    const p = document.createElement("p");
+    p.classList.add("mb-2", "mt-4", "fs-2", "fw-bold");
+    const p2 = document.createElement("p");
+    p2.classList.add("mt-2");
+    const p3 = document.createElement("p");
+    p3.classList.add("mx-auto", "mt-5");
+    p3.style = "border: 2px solid black; width: 10vw; background-color: black";
+    let lstLng = [];
+    for (lang in country.languages) {
+      lstLng.push(country.languages[lang]);
+    }
+    let lstCur = [];
+    for (currencie in country.currencies) {
+      lstCur.push(country.currencies[currencie]);
+    }
+    const p4 = document.createElement("p");
+    lstCur.forEach((cur) => {
+      if (country.capital) {
+        p.innerHTML = `${country.name.common}`;
+        p2.innerHTML = `Capitale : ${country.capital[0]}  <br/>  
+                        Habitants : ${country.population} <br/> 
+                        Nom en francais : ${
+                          country.translations.fra.official
+                        }<br/> 
+                        Superficie : ${country.area} km² <br/> 
+                        Langues : ${lstLng.join(", ")} <br/> 
+                        Monnaie : ${cur["name"]} <br/> 
+                        Symbole : ${cur["symbol"]}`;
+      } else {
+        p.innerHTML = `${country.name.common}`;
+        p2.innerHTML = `Capitale : Pas de capitale  <br/>  
+                        Habitants : ${country.population} <br/> 
+                        Nom en francais : ${
+                          country.translations.fra.official
+                        }<br/> 
+                        Superficie : ${country.area} km² <br/> 
+                        Langues : ${lstLng.join(", ")} <br/> 
+                        Monnaie : ${cur["name"]} <br/> 
+                        Symbole : ${cur["symbol"]}`;
+      }
     });
-}
+    result.appendChild(p);
+    result.appendChild(img);
+    result.appendChild(p2);
+    result.appendChild(p4);
+    console.log(country);
+    openStreetMapsLink = document.createElement("a");
+    openStreetMapsLink.href = country.maps.openStreetMaps;
+    openStreetMapsLink.target = "_blank";
+    openStreetMapsLink.classList.add("btn", "btn-info");
+    openStreetMapsLink.textContent = "Voir sur la carte du monde";
+    result.appendChild(openStreetMapsLink);
+    result.appendChild(p3);
+  });
+};
